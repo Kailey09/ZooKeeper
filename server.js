49e6cff3871c3b5d76d0e03aab6d175ba9d314c2
Kailey09 <kcoop1323@gmail.com>
2022-05-13 15:43:00 -0400

    added test folder

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 const fs = require('fs');
 const path = require('path');
 const { animals } = require('./data/animals.json');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
-app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use(express.static('public'));;
 
 
 
 
 
-  app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, './public/index.html'));
-  });
 
-  app.get('/animals', (req, res) => {
-      res.sendFile(path.join(__dirname, './public/animals.html'));
-  });
 
-  app.get('/zookeepers', (req, res) => {
-      res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-  });
 
-  app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, './public/index.html'));
-  });
 
 
   app.listen(PORT, () => {
